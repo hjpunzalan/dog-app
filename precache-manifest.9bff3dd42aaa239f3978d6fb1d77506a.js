552f6cedc83b8f6c53f2e98ba1030a0fb833ed1b
Jonathan <hj.punzalan@hotmail.com>
2019-05-22 16:16:31 +0800

    Updates

---
+++ precache-manifest.9bff3dd42aaa239f3978d6fb1d77506a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69a4b1f3b00023421a78ce4c5712b5f",
+    "url": "/dog-app/index.html"
+  },
+  {
+    "revision": "9f2f639584b0f5aeabac",
+    "url": "/dog-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ef0af0dbc221d6361ea0",
+    "url": "/dog-app/static/css/main.a7cbf9c2.chunk.css"
+  },
+  {
+    "revision": "9f2f639584b0f5aeabac",
+    "url": "/dog-app/static/js/2.92f892fe.chunk.js"
+  },
+  {
+    "revision": "ef0af0dbc221d6361ea0",
+    "url": "/dog-app/static/js/main.6320b7cb.chunk.js"
+  },
+  {
+    "revision": "13f41eb9af751c6a0405",
+    "url": "/dog-app/static/js/runtime~main.f1bd9a0b.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/dog-app/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/dog-app/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/dog-app/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
